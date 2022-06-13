79e7a110a9cf20207299f994cba58cb478f00f6b
Claire Miller <clairemiller@outlook.com>
2022-06-13 03:07:46 -0700

    updated dropdown on account and settings

--- members/views.py
+++ members/views.py
@@ -7,6 +7,13 @@ from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm
 from thesite.models import Profile
 
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'twitter_url', 'facebook_url', 'instagram_url']
+    success_url = reverse_lazy('home')
+
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
